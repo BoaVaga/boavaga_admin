85084dd1dea950b2dc792f9f0928aa7b85431020
André Carvalho <andrecarvalhoprado@gmail.com>
2021-10-28 18:03:32 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/boavaga_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.boavaga_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,479 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Remover dps, bandeira modo debug
+      title: 'Boa Vaga',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(240, 255, 255, 255), // Ligth
+        // scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255), // Branco
+        // scaffoldBackgroundColor: Color.fromRGBO(35, 35, 35, 1), // Cinza Escuro
+      ),
+      home: MyHomePage(), // AQUI
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  Home createState() => Home();
+}
+
+class Home extends State<MyHomePage> {
+  bool isSwitched = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/logonometransparente.png',
+              fit: BoxFit.contain,
+              height: 70,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 40, right: 20, left: 20),
+              child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                      fillColor: Color.fromARGB(20, 20, 20, 20),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.person,
+                        size: 32.0,
+                        color: Colors.grey.shade800,
+                      ),
+                      hintText: 'Usuário')),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20, right: 20, left: 20),
+              child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                      fillColor: Color.fromARGB(20, 20, 20, 20),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        size: 30.0,
+                        color: Colors.grey.shade800,
+                      ),
+                      hintText: 'Senha')),
+            ),
+            Container(
+                margin: const EdgeInsets.only(top: 30, left: 20),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text('Entrar como admin:',
+                          style: TextStyle(fontSize: 16)),
+                    ),
+                  ],
+                )),
+            Container(
+                margin: const EdgeInsets.only(right: 20, left: 20),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text('Estacionamento',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 17)),
+                    ),
+                    Switch(
+                      value: isSwitched,
+                      onChanged: (value) {
+                        setState(() {
+                          isSwitched = value;
+                          print(isSwitched);
+                        });
+                      },
+                    ),
+                    Expanded(
+                      child: Text('Sistema',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 17)),
+                    ),
+                  ],
+                )),
+            new SizedBox(
+                width: 200.0,
+                child: Container(
+                    margin: const EdgeInsets.only(top: 30),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          // AQUI Login
+                          if (isSwitched == false) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      HomeAdmEstacionamento()),
+                            );
+                          } else if (isSwitched == true) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomeAdmSistema()),
+                            );
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 15, right: 30, left: 30, bottom: 15),
+                          child: Text('ENTRAR'),
+                        )))),
+            new SizedBox(
+                width: 200.0,
+                child: Container(
+                    margin: const EdgeInsets.only(top: 5),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          // AQUI cadastro
+                          print("cadastro aqui");
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 15, right: 30, left: 30, bottom: 15),
+                          child: Text('CADASTRE-SE'),
+                        )))),
+            Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: InkWell(
+                  child: Text("Esqueci minha senha",
+                      style: TextStyle(fontSize: 15)),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => PgEmailEsqueciSenha()),
+                    );
+                  },
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PgEmailEsqueciSenha extends StatelessWidget {
+  const PgEmailEsqueciSenha({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logonometransparente.png',
+              fit: BoxFit.contain,
+              height: 40,
+            ),
+          ],
+        ),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 40, left: 40),
+              child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                      fillColor: Color.fromARGB(20, 20, 20, 20),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.person,
+                        size: 32.0,
+                        color: Colors.grey.shade800,
+                      ),
+                      hintText: 'Usuário ou Email')),
+            ),
+            Container(
+                margin: const EdgeInsets.only(top: 30),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PgEsqueciSenha()),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 15, right: 30, left: 30, bottom: 15),
+                      child: Text('Continuar'),
+                    ))),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PgEsqueciSenha extends StatelessWidget {
+  const PgEsqueciSenha({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logonometransparente.png',
+              fit: BoxFit.contain,
+              height: 40,
+            ),
+          ],
+        ),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Text(
+                  "Foi enviada uma nova senha para seu email, efetue o login utilizando essa senha!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18)),
+            ),
+            Container(
+                margin: const EdgeInsets.only(top: 30),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyHomePage()),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 15, right: 30, left: 30, bottom: 15),
+                      child: Text('Login'),
+                    ))),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeAdmEstacionamento extends StatelessWidget {
+  const HomeAdmEstacionamento({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logonometransparente.png',
+                fit: BoxFit.contain,
+                height: 40,
+              ),
+            ],
+          ),
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              new SizedBox(
+                  width: 300.0,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // AQUI alterar email
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, right: 30, left: 30, bottom: 15),
+                            child: Text('Alterar meu email'),
+                          )))),
+              new SizedBox(
+                  width: 300.0,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // AQUI alterar email
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, right: 30, left: 30, bottom: 15),
+                            child: Text('Alterar minha senha'),
+                          )))),
+              new SizedBox(
+                  width: 300.0,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // AQUI alterar email
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, right: 30, left: 30, bottom: 15),
+                            child: Text('Editar estacionamento'),
+                          )))),
+              new SizedBox(
+                  width: 300.0,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // AQUI alterar email
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, right: 30, left: 30, bottom: 15),
+                            child: Text('Atualizar vagas manualmente'),
+                          )))),
+            ])));
+  }
+}
+
+class HomeAdmSistema extends StatelessWidget {
+  const HomeAdmSistema({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logonometransparente.png',
+                fit: BoxFit.contain,
+                height: 40,
+              ),
+            ],
+          ),
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              new SizedBox(
+                  width: 300.0,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // AQUI alterar email
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, right: 30, left: 30, bottom: 15),
+                            child: Text('Alterar meu email'),
+                          )))),
+              new SizedBox(
+                  width: 300.0,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // AQUI alterar email
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, right: 30, left: 30, bottom: 15),
+                            child: Text('Alterar minha senha'),
+                          )))),
+              new SizedBox(
+                  width: 300.0,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // AQUI alterar email
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, right: 30, left: 30, bottom: 15),
+                            child: Text('Listar Estacionamentos'),
+                          )))),
+              new SizedBox(
+                  width: 300.0,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // AQUI alterar email
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, right: 30, left: 30, bottom: 15),
+                            child: Text('Analisar novos estacionamentos'),
+                          )))),
+              new SizedBox(
+                  width: 300.0,
+                  child: Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // AQUI alterar email
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, right: 30, left: 30, bottom: 15),
+                            child: Text('Cadastrar novo Admin Sistema'),
+                          )))),
+            ])));
+  }
+}
